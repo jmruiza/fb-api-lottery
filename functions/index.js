acf4e9cd62a00673355553b8711625b66654bd1f
jm_surface <jmruiz.dev@gmail.com>
2022-06-04 12:37:04 -0500

    feat: get user by id

--- functions/index.js
+++ functions/index.js
@@ -15,9 +15,23 @@ app.get('/hello-world', (req, res) => {
 });
 
 app.post('/api/user', async (req, res) => {
-    const {name, email} = req.body;
-    await db.collection('users').doc().create({ name, email })
-    return res.status(200).json({message: 'User created!'});
+    try {
+        const {name, email} = req.body;
+        await db.collection('users').doc().create({ name, email })
+        return res.status(200).json({message: 'User created!'});
+    } catch (error) {
+        return res.status(500).json({message: error.message});
+    }
+});
+
+app.get('/api/users/:user_id', async (req, res) => {
+    try {   
+        const {user_id} = req.params;
+        const user = await db.collection('users').doc(user_id).get();
+        return res.status(200).json(user.data());
+    } catch (error) {
+        return res.status(500).json({message: error.message});
+    }
 });
 
 
