20fc4726cdf402d5c23f8bccbaef3422b9ab7820
JM (Emerald Digital) <jmanuel.rar@emeraldigital.com>
2022-06-10 11:27:54 -0500

    feat: add basic test

--- functions/app.js
+++ functions/app.js
@@ -1,16 +1,20 @@
-const express = require("express");
-const admin = require("firebase-admin");
+import express from "express";
+import admin from "firebase-admin";
 
 const app = express();
 
 admin.initializeApp({
-    credential: admin.credential.cert("./permissions.json")
+    credential: admin.credential.cert("./permissions.json"),
 });
 
-const db = admin.firestore();
+// const db = admin.firestore();
 
-app.get('/hello-world', (req, res) => {
-    return res.status(200).json({'message':'Hello World!'});
+app.get("/hello-world", (req, res) => {
+    try {
+        return res.status(200).json({"message": "Hello World!"});
+    } catch (error) {
+        return res.status(500);
+    }
 });
 
-module.exports = app; 
+module.exports = app;
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
-const functions = require("firebase-functions");
-const app = require("./app");
+import functions from "firebase-functions";
+import app from "../app";
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000');
+    console.log("Server is listening on port 3000");
 });
 
 exports.app = functions.https.onRequest(app);
