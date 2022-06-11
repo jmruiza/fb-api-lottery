73e089f731187ea2d135d3abe5ea73191660fab7
JM (Emerald Digital) <jmanuel.rar@emeraldigital.com>
2022-06-11 12:40:42 -0500

    feat: add bodyParser package

--- functions/app.js
+++ functions/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
+// Middlewares
+app.use(bodyParser.json());
+         
+// Routes
 app.get("/hello-world", (req, res) => {
     try {
         return res.status(200).json({"message": "Hello World!"});
